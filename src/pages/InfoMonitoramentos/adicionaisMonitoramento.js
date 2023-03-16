0a192134c34f60cf4dbfbbf91ff48c690f214006
Natanna <erzafairy79@gmail.com>
2023-03-16 12:23:59 -0300

    pasta com as paginas de monitoramento, produtores, propriedade e vinculos

--- src/pages/InfoMonitoramentos/adicionaisMonitoramento.js
+++ src/pages/InfoMonitoramentos/adicionaisMonitoramento.js
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom';
 
+
 // Componente da pagina de mais informações sobre os Monitoramentos
 const AdicionalMonitoramento = (props) => {
     // Buscar o ID que esta vindo da URL
